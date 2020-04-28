fc20c2dab4d2b8580c6b6b03731e9a222fd553a3
Oscar Marie--Taillefer <oscar.marie-taillefer@spikeelabs.fr>
2020-04-28 12:07:07 +0200

    Update to include more dep

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,17 @@
 ARG python_version
-FROM python:$python_version
-ARG driver_version
-RUN apt-get update &&\
-    apt-get install libev4 libev-dev
-RUN pip install cassandra-driver==$driver_version
+
+FROM python:${python_version}
+
+RUN apt-get update && apt-get install -y curl apt-transport-https gnupg2 jq \
+    && curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add - \
+    && curl https://packages.microsoft.com/config/debian/8/prod.list > /etc/apt/sources.list.d/mssql-release.list \
+    && apt-get update \
+    && ACCEPT_EULA=Y apt-get install -y msodbcsql17 mssql-tools \
+    && apt-get install -y libev4 libev-dev unixodbc-dev
+
+# Preinstall pip libs to use cassandra, kafka, mongo and sql (spikeelabs default libs)
+RUN pip install cassandra-driver==3.7.1 confluent-kafka==1.4.1  pymongo==3.10.1 pyodbc==4.0.26
+
+# Install poetry (using install recommended way) and setup poetry path
+RUN curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
+ENV PATH="${PATH}:/root/.poetry/bin"
--- create-images.sh
+++ create-images.sh
@@ -1,33 +1,28 @@
 #!/usr/bin/env bash
-driver_versions=();
 python_versions=();
 
 IFS=$'\n'
-driver_versions=(`cat $1`)
-python_versions=(`cat $2`)
+python_versions=(`cat $1`)
 IFS=$' \t\n'
 
-for driver_version in "${driver_versions[@]}"
+for python_version_aliases in "${python_versions[@]}"
 do
-    for python_version_aliases in "${python_versions[@]}"
+    python_version_aliases=(${python_version_aliases})
+    tags_args=()
+    for python_version in "${python_version_aliases[@]}"
     do
-        python_version_aliases=(${python_version_aliases})
-        tags_args=()
-        for python_version in "${python_version_aliases[@]}"
-        do
-            if [[ ${python_version} != latest ]]; then
-                tags_args+=(-t "benbk/python-cassandra-driver:$python_version-$driver_version")
-            fi
-            if [[ ${driver_versions[0]} == ${driver_version} ]]; then
-                tags_args+=(-t "benbk/python-cassandra-driver:$python_version")
-            fi
-        done
-        docker build --pull --build-arg python_version="${python_version}" --build-arg driver_version="$driver_version" "${tags_args[@]}" .
-        for i in "${!tags_args[@]}"
-        do
-            if [[ $((i%2)) -eq 1 ]]; then
-                docker push ${tags_args[i]}
-            fi
-        done
+        if [[ ${python_version} != latest ]]; then
+            tags_args+=(-t "benbk/spk-python:$python_version")
+        fi
+        if [[ ${driver_versions[0]} == ${driver_version} ]]; then
+            tags_args+=(-t "benbk/spk-python:$python_version")
+        fi
+    done
+    docker build --pull --build-arg python_version="${python_version}" --build-arg "${tags_args[@]}" .
+    for i in "${!tags_args[@]}"
+    do
+        if [[ $((i%2)) -eq 1 ]]; then
+            docker push ${tags_args[i]}
+        fi
     done
 done
